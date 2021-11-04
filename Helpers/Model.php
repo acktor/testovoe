45a318b4acba6b207d1d82b4857629dcfa3f3aa8
acktor <bahtiar1993@gmail.com>
2021-11-04 20:20:57 +0600

    done

--- Helpers/Model.php
+++ Helpers/Model.php
@@ -3,6 +3,8 @@
 namespace Helpers;
 
 use Helpers\Database;
+use ReflectionObject;
+use ReflectionProperty;
 
 
 class Model
@@ -14,15 +16,17 @@ class Model
     protected $_whereArr = [];
     protected $_attributes = [];
 
-    public function __construct($_table) {
+    public function __construct($_table)
+    {
         $this->_db = new Database();
         $this->_table = $_table;
     }
 
-    public function get() {
+    public function get()
+    {
         $query_str = 'SELECT * FROM ' . $this->_table;
 
-        if (!empty($this->_whereStr)){
+        if (!empty($this->_whereStr)) {
             $query_str .= $this->_whereStr;
         }
 
@@ -31,10 +35,11 @@ class Model
         return $result;
     }
 
-    public function first() {
+    public function first()
+    {
         $query_str = 'SELECT * FROM ' . $this->_table;
 
-        if (!empty($this->_whereStr)){
+        if (!empty($this->_whereStr)) {
             $query_str .= $this->_whereStr;
         }
 
@@ -48,24 +53,42 @@ class Model
             return null;
         }
 
-        foreach($result[0] as $key => $value){
+        foreach ($result[0] as $key => $value) {
             $this->{$key} = $value;
             $this->_attributes[$key] = $value;
         }
 
     }
 
-    public function save(){
-        if (!empty($this->id)){     //update
+    public function save()
+    {
+        $costil = ''; //???
 
-        } else {        //insert
+        $properties = (new ReflectionObject($this))->getProperties(ReflectionProperty::IS_PUBLIC);
 
+        foreach ($properties as $property) {
+            $columns[$property->name] = $this->{$property->name};
+            $costil .= '?,';
         }
+
+//        $this->id = 1;
+
+//        var_dump(substr_count(count($columns), ","));die;
+//
+        if (!empty($this->id)) {     //update
+            $query_str = 'UPDATE ' . $this->_table . ' SET ' .  implode(' = ?, ', array_keys($columns)). ' = ?' . ' where id = ' . $this->id;
+        } else {        //insert
+            $query_str = 'INSERT INTO ' . $this->_table . '(' . implode(',', array_keys($columns)) . ') VALUES (' . substr($costil,0,-1) . ')';
+        }
+
+        $this->_db->query($query_str, array_values($columns));
+
     }
 
-    public function where($column, $operator, $data) {
+    public function where($column, $operator, $data)
+    {
 
-        if (!empty($this->_whereArr)){
+        if (!empty($this->_whereArr)) {
             $prefix = 'and';
         } else {
             $prefix = 'where';
--- banner.php
+++ banner.php
@@ -3,7 +3,7 @@
 use Helpers\Image;
 use Helpers\Model;
 
-spl_autoload_register(function($className) {
+spl_autoload_register(function ($className) {
     $file = __DIR__ . '\\' . $className . '.php';
     $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
     if (file_exists($file)) {
@@ -21,20 +21,23 @@ $attrubutes = ['ip_address' => 123];
 
 $banner->where('ip_address', '=', $_SERVER['REMOTE_ADDR']);
 $banner->where('user_agent', '=', $_SERVER['HTTP_USER_AGENT']);
-$banner->where('page_url', '=', $_SERVER['REQUEST_URI']);
+$banner->where('page_url', '=', $_SERVER['HTTP_REFERER']);
 
 $banner->first();
+
+//var_dump($_SERVER['REQUEST_URI']);die;
 
 if (empty($banner->id)) {
     $banner->ip_address = $_SERVER['REMOTE_ADDR'];
     $banner->user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $banner->page_url = $_SERVER['REQUEST_URI'];
+    $banner->page_url = $_SERVER['HTTP_REFERER'];
     $banner->views_count = 1;
 } else {
-    $banner->views_count = $banner->views_count++;
+    $banner->views_count = $banner->views_count+1;
 }
 
-//$banner->views_count = $banner->views_count++;
+$banner->view_date = date('Y-m-d H:i:s');
+
 $banner->save();
 
 //imagejpeg(imagecreatefromjpeg($image->getUrl()));
